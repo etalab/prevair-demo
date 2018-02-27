3e184dba52c6650b1c52797255924c351a6559ac
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-02-27 20:43:39 +0100

    Simplify commune page

--- pages/commune.js
+++ pages/commune.js
@@ -1,7 +1,4 @@
-import FaSearch from 'react-icons/lib/fa/search'
-
 import Page from '../layouts/main'
-import Head from '../components/main/head'
 
 import Section from '../components/section'
 import Commune from '../components/commune'
@@ -11,9 +8,6 @@ const description = 'Découvrez la qualité de l’air de votre commune'
 
 export default () => (
   <Page title={title} description={description}>
-    <Head title={title} icon={<FaSearch />}>
-      {description}
-    </Head>
     <Section>
       <Commune />
     </Section>
