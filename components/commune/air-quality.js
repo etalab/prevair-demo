e9457882027bc142dea6738737b63e298d7251de
tmerlier <tmerlier@student.42.fr>
2018-02-27 11:49:30 +0100

    Fix degressive evolution arrow’s direction.

--- components/commune/air-quality.js
+++ components/commune/air-quality.js
@@ -78,6 +78,10 @@ const AirQuality = ({...props}) => {
 
         td.green {
           background: ${theme.successBg};
+          -moz-transform: scaleX(-1);
+          -o-transform: scaleX(-1);
+          -webkit-transform: scaleX(-1);
+          transform: scaleX(-1);
         }
 
         td.red {
